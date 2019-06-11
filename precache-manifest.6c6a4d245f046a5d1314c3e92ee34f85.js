4408f030e5d4cb8445879b0c71f1bc4a17e9a432
franciscoescobar <franciscoescobar@users.noreply.github.com>
2019-06-11 13:20:27 -0300

    Initial commit

---
+++ precache-manifest.6c6a4d245f046a5d1314c3e92ee34f85.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fccaae31c1b091bda3f972832810c0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9b39d7f572be8189f7c",
+    "url": "/static/css/main.5ec08057.chunk.css"
+  },
+  {
+    "revision": "74a1b723a53213f9d338",
+    "url": "/static/js/2.8c72d9b7.chunk.js"
+  },
+  {
+    "revision": "c9b39d7f572be8189f7c",
+    "url": "/static/js/main.889c9023.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6a45e5de2c8d8cc1e07abf252ef52fa6",
+    "url": "/static/media/AGUA-SUMATE.6a45e5de.jpg"
+  },
+  {
+    "revision": "b90fd6362e8dad9b690f7ef05b53ccbb",
+    "url": "/static/media/enviar.b90fd636.svg"
+  },
+  {
+    "revision": "f2be5f618b7936cd017e587fafd11315",
+    "url": "/static/media/finalizado.f2be5f61.svg"
+  },
+  {
+    "revision": "8ba8cbed1fb38d078ddb27474560921f",
+    "url": "/static/media/huella.8ba8cbed.svg"
+  },
+  {
+    "revision": "1f43d02bab2ff2be0f73d1dedae1dcb7",
+    "url": "/static/media/lapiz.1f43d02b.svg"
+  },
+  {
+    "revision": "8b32ee1c1673340efbc96f717f577ee0",
+    "url": "/static/media/logo.8b32ee1c.svg"
+  },
+  {
+    "revision": "5132af158b190ded3e4faad0c5d0045b",
+    "url": "/static/media/mano.5132af15.svg"
+  },
+  {
+    "revision": "f7b31a19211421792efe1fc6006ba3d7",
+    "url": "/static/media/ocean.f7b31a19.mp4"
+  },
+  {
+    "revision": "921dab27ca6e370eb1ae93b88e006d49",
+    "url": "/static/media/sumate.921dab27.svg"
+  }
+]);
